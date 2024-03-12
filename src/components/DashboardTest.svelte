91e07a295624f85f9945c40959a1504fc53e57b8
andre-brandao <brandaoandre00@gmail.com>
2024-03-12 19:43:24 -0300

    grafico de febre

--- src/components/DashboardTest.svelte
+++ src/components/DashboardTest.svelte
@@ -64,101 +64,124 @@
       statisticType: "sum",
     },
   ];
+
+  let charts = [
+    {
+      chart: {
+        type: "bar",
+        data: {
+          labels: [
+            "sem_epi0",
+            "sem_epi1",
+            "sem_epi2",
+            "sem_epi3",
+            "sem_epi4",
+            "sem_epi5",
+            "sem_epi6",
+            "sem_epi7",
+            "sem_epi8",
+            "sem_epi9",
+          ],
+          datasets: [
+            {
+              label: "Casos Notificados",
+              backgroundColor: "#149dcf",
+              stack: "Stack 0",
+              data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            },
+          ],
+        },
+        options: {
+          responsive: false,
+          legend: {
+            position: "top",
+          },
+        },
+      },
+      sql_filter: filter_sem_epi,
+    },
+    {
+      chart: {
+        type: "bar",
+        data: {
+          labels: [
+            "0 a 1",
+            "1 a 4",
+            "5 a 14",
+            "15 a 29",
+            "30 a 39",
+            "40 a 59",
+            "60 ou mais",
+          ],
+          datasets: [
+            {
+              label: "Idade",
+              backgroundColor: "#149dcf",
+              stack: "Stack 0",
+              data: [0, 0, 0, 0, 0, 0, 0],
+            },
+          ],
+        },
+      },
+      sql_filter: filter_idade,
+    },
+    // febre
+    {
+      chart: {
+        type: "doughnut",
+        data: {
+          labels: ["FEBRE = 1", "FEBRE = 2"],
+          datasets: [
+            {
+              backgroundColor: ["#149dcf", "#ed5050"],
+              borderColor: "rgb(255, 255, 255)",
+              borderWidth: 1,
+              data: [0, 0],
+            },
+          ],
+        },
+        options: {
+          responsive: false,
+          cutoutPercentage: 35,
+          legend: {
+            position: "bottom",
+          },
+          title: {
+            display: true,
+            text: "Comorbidades",
+          },
+        },
+      },
+      sql_filter: [
+        {
+          onStatisticField: "CASE WHEN FEBRE = 1 THEN 1 ELSE 0 END",
+          outStatisticFieldName: "febre_sim",
+          statisticType: "sum",
+        },
+        {
+          onStatisticField: "CASE WHEN FEBRE = 2 THEN 1 ELSE 0 END",
+          outStatisticFieldName: "febre_nao",
+          statisticType: "sum",
+        },
+      ],
+      size:{
+        width: 200,
+        height: 200
+      }
+    },
+  ];
+
   let mapDrad: MapWrapperParams = {
     mapID: mapId,
     options: {
       filter: {
-        fieldNames: ["SEMANA_EPI", "IDADE"],
+        fieldNames: ["SEMANA_EPI", "IDADE", "FEBRE"],
         radius: 1,
         // sql_filter: filter_sem_epi,
         // where: "CLASSIFICACAO_FINAL = 'Confirmado'"
       },
     },
-    charts: [
-      {
-        chart: {
-          type: "bar",
-          data: {
-            labels: [
-              "sem_epi0",
-              "sem_epi1",
-              "sem_epi2",
-              "sem_epi3",
-              "sem_epi4",
-              "sem_epi5",
-              "sem_epi6",
-              "sem_epi7",
-              "sem_epi8",
-              "sem_epi9",
-            ],
-            datasets: [
-              {
-                label: "Casos Notificados",
-                backgroundColor: "#149dcf",
-                stack: "Stack 0",
-                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
-          },
-          options: {
-            responsive: false,
-            legend: {
-              position: "top",
-            },
-            title: {
-              display: true,
-              text: "Casos por mês/ano",
-            },
-            scales: {
-              //   x: {
-              //     ticks: {
-              //       callback: function (value, index, ticks) {
-              //         // console.log(ticks);
-              //         return value;
-              //       },
-              //     },
-              //   },
-              // yAxes: [
-              //   {
-              //     stacked: true,
-              //     ticks: {
-              //       beginAtZero: true,
-              //     },
-              //   },
-              // ],
-            },
-          },
-        },
-
-        sql_filter: filter_sem_epi,
-      },
-
-      {
-        chart: {
-          type: "bar",
-          data: {
-            labels: [
-              "0 a 1",
-              "1 a 4",
-              "5 a 14",
-              "15 a 29",
-              "30 a 39",
-              "40 a 59",
-              "60 ou mais",
-            ],
-            datasets: [
-              {
-                label: "Idade",
-                backgroundColor: "#149dcf",
-                stack: "Stack 0",
-                data: [0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
-          },
-        },
-        sql_filter: filter_idade,
-      },
-    ],
+    charts,
   };
 
   let scenePoly: SceneWrapperParams = {
@@ -172,73 +195,13 @@
         // where: "CLASSIFICACAO_FINAL = 'Confirmado'"
       },
     },
-    charts: [
-      {
-        chart: {
-          type: "bar",
-          data: {
-            labels: [
-              "sem_epi0",
-              "sem_epi1",
-              "sem_epi2",
-              "sem_epi3",
-              "sem_epi4",
-              "sem_epi5",
-              "sem_epi6",
-              "sem_epi7",
-              "sem_epi8",
-              "sem_epi9",
-            ],
-            datasets: [
-              {
-                label: "Casos Notificados",
-                backgroundColor: "#149dcf",
-                stack: "Stack 0",
-                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
-          },
-          options: {
-            responsive: false,
-            legend: {
-              position: "top",
-            },
-          },
-        },
-        sql_filter: filter_sem_epi,
-      },
-      {
-        chart: {
-          type: "bar",
-          data: {
-            labels: [
-              "0 a 1",
-              "1 a 4",
-              "5 a 14",
-              "15 a 29",
-              "30 a 39",
-              "40 a 59",
-              "60 ou mais",
-            ],
-            datasets: [
-              {
-                label: "Idade",
-                backgroundColor: "#149dcf",
-                stack: "Stack 0",
-                data: [0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
-          },
-        },
-        sql_filter: filter_idade,
-      },
-    ],
+    charts,
   };
 </script>
 
 <div class="w-full flex justify-between p-2 bg-gray-200">
   <p class="font-bold underline text-2xl">Dengue 2024 João Monlevade</p>
-  <div class="bg-primary  p-2">
+  <div class="bg-primary p-2">
     <label for="map-type">Tipo de Mapa</label>
     <select name="map-type" id="map-type" bind:value={select_value}>
       <option value="drag">Arraste para buscar</option>

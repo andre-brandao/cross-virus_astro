74d0fcd69d4adf1f8c7ba59ff06ae3c75bd28a2c
andre-brandao <brandaoandre00@gmail.com>
2024-03-18 19:09:45 -0300

    Update output to 'server' and add type annotations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'hybrid',
+  output:'server',
   integrations: [svelte(), tailwind()],
   adapters: [vercel()],
 });

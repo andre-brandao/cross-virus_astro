f9effb0673b696fff87dcef19313d2c0f820c422
andre-brandao <brandaoandre00@gmail.com>
2024-03-20 16:13:59 -0300

    api google

--- src/pages/api/geoencode/from_adress.ts
+++ src/pages/api/geoencode/from_adress.ts
@@ -1,62 +1,53 @@
 import type { APIRoute } from "astro";
-import { parse } from "csv-parse/sync"; // Certifique-se de instalar esta biblioteca
+import { parse } from "csv-parse/sync";
+import { stringify } from "csv-stringify/sync";
+import { Client } from "@googlemaps/google-maps-services-js";
 
 export const POST: APIRoute = async ({ request }) => {
-  // Recebe o arquivo CSV e o nome do campo contendo o endereço
   const formData = await request.formData();
   const file = formData.get("csv");
-  const addressField = formData.get("field");
-
-  console.log(file, typeof file);
-
-  console.log(addressField);
+  const addressField = formData.get("field")?.toString();
 
   if (!file || !(file instanceof File) || typeof addressField !== "string") {
     return new Response("Arquivo ou campo de endereço inválido", {
       status: 400,
     });
   }
+  console.log(file, addressField);
 
-  // Lê e analisa o arquivo CSV
   const csvContent = await file.text();
   const records = parse(csvContent, {
     columns: true,
     skip_empty_lines: true,
   });
 
-  // Geocodifica cada endereço e coleta as coordenadas
-  const results = await Promise.all(
-    records.map(async (record) => {
-      const address = record[addressField];
-      const encodedAddress = encodeURIComponent(address);
+  const client = new Client({});
 
-      try {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${
-            import.meta.env.GOOGLE_MAPS_KEY
-          }`
-        );
-        const data = await response.json();
-        console.log("response google api", data);
-        return {
-          address,
-          location: data.results[0]?.geometry.location,
-        };
-      } catch (error) {
-        console.error(error);
-        return {
-          address,
-          location: null,
-          error: error.message,
-        };
-      }
-    })
-  );
+  // Geocodifica cada endereço e armazena o resultado
+  for (const record of records) {
+    const address = record[addressField];
 
-  console.log(results);
+    if (address) {
+      console.log(address);
+      const result = await client.geocode({
+        params: {
+          address: address,
+          key: import.meta.env.GOOGLE_MAPS_KEY,
+        },
+      });
 
-  // Retorna as coordenadas geocodificadas
-  return new Response(JSON.stringify(results), {
-    headers: { "Content-Type": "application/json" },
+      const location = result.data.results[0]?.geometry.location;
+      record["Latitude"] = location?.lat ?? null;
+      record["Longitude"] = location?.lng ?? null;
+    }
+  }
+  // console.log(records);
+
+  // Converte os dados atualizados de volta para o formato CSV
+  const updatedCsvContent = stringify(records, { header: true });
+
+  // Retorna o CSV atualizado
+  return new Response(updatedCsvContent, {
+    headers: { "Content-Type": "text/csv" },
   });
 };

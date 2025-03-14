f935eaacaa2aacc178dcb2e898ef6e9c7c663cc2
André de Oliveira Brandão <82166576+andre-brandao@users.noreply.github.com>
2025-03-14 10:53:02 -0300

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,1 +1,1 @@
-export let wpp_link = "https://wa.me/5531983861852?text=Me%20passe%20mais%20informações%20sobre%20o%20Crossvirus";
+export let wpp_link = "https://wa.me/5531998085422?text=Me%20passe%20mais%20informações%20sobre%20o%20Crossvirus";

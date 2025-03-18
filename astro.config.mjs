d91023f44ac900192b940ccc1497cd9d97e264d8
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-03-18 20:34:29 -0300

    node?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   //output: "server",
   integrations: [svelte(), tailwind()],
+  adapter: node({
+    mode: 'standalone'
+  }),
   //adapter: vercel({
   //  webAnalytics: {
   //    enabled: true,

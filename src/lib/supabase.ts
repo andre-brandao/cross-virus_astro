f860e43638f7330f95c9abe9a13ab483d5fdf7d2
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-03-18 19:30:58 -0300

    remove supa

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { type Database } from "./supabase-types";
-export const supabase = createClient<Database>(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
-);
